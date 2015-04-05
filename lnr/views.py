d4fe323f30533f02bc106f2c97a38b33280ab8cf
Robin <42406671@qq.com>
2015-04-05 12:08:17 +0800

    完成注册功能

--- lnr/views.py
+++ lnr/views.py
@@ -1,17 +1,52 @@
 # -*- coding: utf8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from lnr.models import User
 
-import Image, ImageDraw, ImageFont, ImageFilter, random
-
+import Image, ImageDraw, ImageFont, ImageFilter, random#PIL插件的文件
 from hashlib import sha1
 from datetime import datetime
-import cStringIO
-import platform
+import cStringIO#用于把生成的图片写入内存
+import platform#用于判断操作系统
+import json
 
 # Create your views here.
 def regist(request):
-    email = request.POST['a']
+    email = request.POST['email']
+    username = request.POST['username']
+    password = request.POST['password']
+    valicode = request.POST['valicode']
+
+    user_exist = User.objects.filter(UserAccount__exact=email)
+    if user_exist:
+        jsonObject = json.dumps({'email':'邮箱已存在!'},ensure_ascii = False)
+        #加上ensure_ascii = False，就可以保持utf8的编码，不会被转成unicode
+        return HttpResponse(jsonObject,content_type="application/json")
+    
+    name_exist = User.objects.filter(UserName__exact=username)
+    if name_exist:
+        jsonObject = json.dumps({'username':'用户名已存在!'},ensure_ascii = False)
+        #加上ensure_ascii = False，就可以保持utf8的编码，不会被转成unicode
+        return HttpResponse(jsonObject,content_type="application/json")
+
+    if valicode != request.session['CAPTCHA']:
+        jsonObject = json.dumps({'valicode':'验证码错误!'},ensure_ascii = False)
+        #加上ensure_ascii = False，就可以保持utf8的编码，不会被转成unicode
+        return HttpResponse(jsonObject,content_type="application/json")
+
+    del request.session['CAPTCHA']
+    nt = datetime.now()
+    shpw = sha1()
+    shpw.update(password + str(nt)[0:19])
+    pw = shpw.hexdigest()
+    userobj = User()
+    userobj.UserAccount = email
+    userobj.UserName = username
+    userobj.UserPassword = pw
+    userobj.UserPermission = 1
+    userobj.Time = nt
+    userobj.LastLoginTime = nt
+    userobj.save()
     return HttpResponse('regist')
 
 
