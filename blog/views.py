75076f448feb504124fe5c895a7dad42eaa43adf
Robin <42406671@qq.com>
2015-04-08 21:55:28 +0800

    在index视图函数中添加了测试代码。

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render_to_response
 # Create your views here.
 def index(request):
     username = request.session.get('username', '')
+    print '(',username,')'
     if username == '':
+        print '---->1'
         return render_to_response('index.html', {'logined':username})
     else:
+        print '---->2'
         return render_to_response('index.html', {'logined':username})
