6e3f51ae53efb0d566d1d6357f1631972ca0ff74
Trzinory <tarazedinory@gmail.com>
2015-04-14 10:44:59 +0800

    writting：修正返回按钮的实现
    返回index页面

--- blog/static/js/writting.js
+++ blog/static/js/writting.js
@@ -4,9 +4,7 @@ window.onload=function(){
 	//顶栏
 	//返回按钮
 	$("#backwards").click(function(){
-		if(window.history.back()){}
-		else 
-			window.open("./index.html","_self");
+		window.open("./index","_self");
 	});
 	//用户功能
 	$("#list_top li:eq(0)").mouseover(function(){
