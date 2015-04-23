ac035d25f36ac3cb1778798aadcbc8f0e7de71be
Trzinory <tarazedinory@gmail.com>
2015-04-23 10:43:09 +0800

    article：文章id和请求地址一体化

--- blog/static/js/article.js
+++ blog/static/js/article.js
@@ -41,10 +41,6 @@ window.onload=function(){
 		$("#list_logout img").attr({"src":src2});
 	});
 	$("#list_edit").click(function(){
-		var pasid=document.createElement("input");
-		pasid.type="text";
-		pasid.name="passageId";
-		pasid.value=location.pathname.replace(/\/passage\//,"");
 		var form=document.createElement("form");
 		form.method="get";
 		form.action=$("#article").attr("data-url");
