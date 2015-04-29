a098d2c64638c45d5d737c29be76124192a55dbf
Trzinory <tarazedinory@gmail.com>
2015-04-29 12:05:54 +0800

    setting：修改检测逻辑和发送逻辑，成功后退出登录
    setting：增加提醒信息

--- blog/static/js/settings.js
+++ blog/static/js/settings.js
@@ -99,114 +99,146 @@ window.onload=function(){
 		flag[i]=1;
 	}
 	var pattern1=/[^!-z]/,pattern2=/^[\u4e00-\u9fa5A-Za-z0-9_]+$/;
+	var pi1=$(".set_password:eq(0)"),pi2=$(".set_password:eq(1)"),pi3=$(".set_password:eq(2)");
 	$("#uname_input").on({
 		"input":function(){
 			var str=$(this).val();
 			if(str&&!pattern2.test(str)){
 				$("#uname_row .hint").text("字符有误");
-				flag[3]=0;
+				flag[0]=0;
 			}
 			else {
 				$("#uname_row .hint").text("");
-				flag[3]=1;
+				flag[0]=1;
 			}
 		},
 		"blur":function(){
 			var str=$(this).val();
-			if(str.length==0){
-				$(this).next().text("值为空");
-				flag[3]=0;
+			if(str.length==0||str==$("#user_name").text()){
+				$("#user_name").show();
+				$("#uname_edit").show();
+				$("#uname_input").hide();
+				flag[0]=1;
 			}
 		}
 	});
-	for(var i=0;i<3;i++){
-		if(i==0){
-			$(".set_password:eq("+i+")").on({
-				"input":function(){
-					var str=$(this).val();
-					if(pattern1.test(str)){
-						$(this).next().text("字符有误");
-						flag[i]=0;
-					}
-					else {
-						$(this).next().text("");
-						flag[i]=1;
-					}
-				},
-				"blur":function(){
-					var str=$(this).val();
-					if(str.length<8&&str.length>0){
-						$(this).next().text("密码长度至少为8位");
-						flag[i]=0;
-					}
+	$(".set_password:eq(0)").on({
+		"input":function(){
+			var str=$(this).val();
+			if(pattern1.test(str)){
+				$(this).next().text("字符有误");
+				flag[1]=0;
+			}
+			else {
+				$(this).next().text("");
+				flag[1]=1;
+				if(pi2.next().text()=="新旧密码相同"){
+					pi2.next().text("");
 				}
-			});
+			}
+		},
+		"blur":function(){
+			var str=$(this).val();
+			if(str.length<8&&str.length>0){
+				$(this).next().text("密码长度至少为8位");
+				flag[1]=0;
+			}
+			else if(str.length==0){
+				if(!pi2.val()&&!pi3.val()){
+					pi2.next().text("");
+					pi3.next().text("");
+				}
+				flag[1]=1;
+			}
 		}
-		else{
-			$(".set_password:eq("+i+")").on({
-				"input":function(){
-					if($(".set_password:eq(1)").next().text()=="新密码两次输入不同！")
-						$(".set_password:eq(1)").next().text("");
-					var str=$(this).val();
-					if(pattern1.test(str)){
-						$(this).next().text("字符有误");
-						flag[i]=0;
-					}
-					else {
-
-						$(this).next().text("");
-						flag[i]=1;
-					}
-				},
-				"blur":function(){
-					var str=$(this).val();
-					if(str.length<8&&str.length>0){
-						$(this).next().text("密码长度至少为8位");
-						flag[i]=0;
-					}
-				}
-			});
+	});
+	$(".set_password:eq(1)").on({"input":function(){checkPas1(2,this)},"blur":function(){checkPas2(2,this)}});
+	$(".set_password:eq(2)").on({"input":function(){checkPas1(3,this)},"blur":function(){checkPas2(3,this)}});
+	function checkPas1(i,t){
+		th=t;
+		if($(".set_password:eq(1)").next().text()=="新密码两次输入不同！")
+			$(".set_password:eq(1)").next().text("");
+		var str=$(th).val();
+		if(pattern1.test(str)){
+			$(th).next().text("字符有误");
+			flag[i]=0;
+		}
+		else {
+			$(th).next().text("");
+			flag[i]=1;
+		}
+	 }
+	function checkPas2(i,t){
+		th=t;
+		var str=$(th).val();
+		if(str.length<8&&str.length>0){
+			$(th).next().text("密码长度至少为8位");
+			flag[i]=0;
+		}
+		if(str.length==0){
+			flag[i]=1;
+		}
+		if(!pi2.val()&&!pi3.val()&&pi1.next().text()=="值为空"){
+			pi1.next().text("");
 		}
 	}
 
 	//ajax请求
 	$("#send").click(function(){
+		var uname=$("#uname_input").val();
 		var pas1=$(".set_password:eq(0)").val();
 		var pas2=$(".set_password:eq(1)").val();
 		var pas3=$(".set_password:eq(2)").val();
-		if(flag[0]&&flag[1]&&flag[2]&&flag[3]){
-			if(pas2!=pas3){
-				$(".set_password:eq(1)").next().text("新密码两次输入不同！")
-			}
-			else if(pas1&&!pas2){
-				$(".set_password:eq(1)").next().text("值为空");
-				$(".set_password:eq(2)").next().text("值为空");
-			}
-			else if(pas2&&!pas1){
-				$(".set_password:eq(0)").next().text("值为空");
-			}
-			else{//*
-				var form=document.getElementById("settings_form");
-				var formdata=new FormData(form);
-				$.ajax({
-					url:"/saveSettings",
-					type:"post",
-					contentType:false,
-					processData:false,
-					data:formdata,
-					success:function(data){
-						if(data.username){
-							$("#uname_row .hint").text(data.username);
-						}
-						if(data.password){
-							$(".set_password:eq(0)").next().text(data.password);
-						}
-					},
-					error:function(){
-						alert("error");
+		var formdata=new FormData(),a=0;
+		if(uname&&uname!=$("#user_name").text()){
+			formdata.append("username",uname);
+			a=1;
+		}
+		if(pas1&&flag[1]&&flag[2]&&flag[3]){
+			formdata.append("opassword",pas1);
+			formdata.append("newpassword1",pas2);
+			formdata.append("newpassword2",pas3);
+			a=1;
+		}
+		if(pas2!=pas3){
+			$(".set_password:eq(1)").next().text("新密码两次输入不同！");
+		}
+		else if(flag[1]&&pas1&&!pas2){
+			$(".set_password:eq(1)").next().text("值为空");
+			$(".set_password:eq(2)").next().text("值为空");
+		}
+		else if(pas2&&!pas1){
+			$(".set_password:eq(0)").next().text("值为空");
+		}
+		else if(pas1&&pas1==pas2){
+			$(".set_password:eq(1)").next().text("新旧密码相同");
+		}
+		else if(a==1&&flag[0]&&flag[1]&&flag[2]&&flag[3]){
+			//*
+			$.ajax({
+				url:"/saveSettings",
+				type:"post",
+				contentType:false,
+				processData:false,
+				data:formdata,
+				success:function(data){
+					if(data.username){
+						$("#uname_row .hint").text(data.username);
 					}
-				});//*/
-			}
+					if(data.password){
+						$(".set_password:eq(0)").next().text(data.password);
+					}
+					if(data.status=="success"){
+						var form=document.createElement("form");
+						form.method="get";
+						form.action="/logout";
+						form.submit();
+					}
+				},
+				error:function(){
+					alert("error");
+				}
+			});//*/
 		}
 	});
 }
